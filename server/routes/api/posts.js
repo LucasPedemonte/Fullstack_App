ad3fbd6f1262be62978c34dca254c3960b3002e1
LucasPedemonte <144968398+LucasPedemonte@users.noreply.github.com>
2024-04-22 19:27:00 -0400

    Your commit message here

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,31 +1,13 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongodb = require("mongodb");
 
 const router = express.Router();
-
-// Set up MongoDB connection URI and options
-const uri =
-  "mongodb+srv://lucasapedemonte:81othJ5ryldcRZfL@cluster0.g7o1od1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-// Get Posts
+//Get Posts
 router.get("/", async (req, res) => {
-  try {
-    const posts = await loadPostsCollection();
-    res.send(await posts.find({}).toArray());
-  } catch (error) {
-    console.error("Error fetching posts", error);
-    res.status(500).send("Failed to load posts");
-  }
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
-
-// Add Post
+//Add Post
 router.post("/", async (req, res) => {
   const posts = await loadPostsCollection();
   await posts.insertOne({
@@ -34,42 +16,16 @@ router.post("/", async (req, res) => {
   });
   res.status(201).send();
 });
-
+//Delete Post
 router.delete("/:id", async (req, res) => {
   const posts = await loadPostsCollection();
-  await posts.deleteOne({ _id: mongodb.ObjectId(req.params.id) });
+  await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
   res.status(200).send();
 });
-
 async function loadPostsCollection() {
-  try {
-    await client.connect();
-    return client.db("vue_express").collection("posts");
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
-    throw error; // Re-throw the error to be caught by the route handler
-  }
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://lucasapedemonte:81othJ5ryldcRZfL@cluster0.g7o1od1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  return client.db("VueExpress").collection("posts");
 }
-
-// Function to close MongoDB connection
-async function closeConnection() {
-  await client.close();
-}
-
 module.exports = router;
-module.exports.closeConnection = closeConnection; // Export this function if needed elsewhere
-
-// Run function to ping MongoDB upon starting the server
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
